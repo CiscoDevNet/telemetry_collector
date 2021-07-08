955dc325394d42122e2ae1a3b75936dbff234953
Shangxin Du <shdu@cisco.com>
2021-07-08 11:39:19 -0700

    change influxdb version to 1.7.11

--- build.sh
+++ build.sh
@@ -10,7 +10,7 @@ export INFLUX_DATA="`pwd`/influxdb"
 export CURRENT_UID=`id -u`
 export CURRENT_GID=`id -g`
 export TELEGRAF_IMAGE="telegraf:latest"
-export INFLUXDB_IMAGE="influxdb:1.8.4"
+export INFLUXDB_IMAGE="influxdb:1.7.11"
 export CHRONOGRAF_IMAGE="chronograf:latest"
 
 self=$0
